d2824654642b6e67b10f336db3388fdd3fdb6c7e
Milen Halachev <milenski88@gmail.com>
2019-09-16 14:14:17 +0300

    Initial Commit

---
+++ TestExistingProject/TestExistingProject.h
@@ -0,0 +1,19 @@
+//
+//  TestExistingProject.h
+//  TestExistingProject
+//
+//  Created by Milen Halachev on 16.09.19.
+//  Copyright © 2019 Milen Halachev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestExistingProject.
+FOUNDATION_EXPORT double TestExistingProjectVersionNumber;
+
+//! Project version string for TestExistingProject.
+FOUNDATION_EXPORT const unsigned char TestExistingProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestExistingProject/PublicHeader.h>
+
+
